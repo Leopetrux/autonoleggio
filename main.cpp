4dadf3f6d7d1df48f932bb4fd7580f1664f001c6
Leopetrux <petruxleonardo@gmail.com>
2023-01-24 18:36:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,32 @@ struct dati_auto
     char lun, mar, mer, gio, ven, sab, dom;
     };
 
+void leggi_file(dati_auto vet_auto[])
+{
+
+    cout<<"Questa è la lista: "<<endl;
+    for (int i=0; i<7; i++)
+    {
+        cout<<endl<<vet_auto[i].categoria<<" "<<vet_auto[i].marca<<" "<<vet_auto[i].modello<<" "<<vet_auto[i].colore<<" ";
+        cout<<" "<<vet_auto[i].lun<<" "<<vet_auto[i].mar<<" "<<vet_auto[i].mer<<" "<<vet_auto[i].gio<<" "<<vet_auto[i].ven<<" "<<vet_auto[i].sab<<" "<<vet_auto[i].dom;
+    }
+}
 void carica_vet(dati_auto vet_autol[])
 {
     ifstream fin("auto.txt", ios::in);
-    string app;
+    string appoggio;
     while(!fin.eof())
     {
         getline(fin,appoggio);
         for(int i=0; i<7; i++)
         {
-            fin>>vet_autol[i].categoria>>vet_autol[i].marca>>vet_autol[i].modello>>vet_autol[i].colore;
+            fin>>vet_auto[i].categoria>>vet_auto[i].marca>>vet_auto[i].modello>>vet_auto[i].colore;
             fin>>vet_auto[i].lun>>vet_auto[i].mar>>vet_auto[i].mer>>vet_auto[i].gio>>vet_auto[i].ven>>vet_auto[i].sab>>vet_auto[i].dom;
         }
     }
     fin.close();
 }
+
 void menu()
 {
     int scelta;
