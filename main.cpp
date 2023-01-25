01b97c9578a01fee74f53851be74155dc6002347
Leopetrux <petruxleonardo@gmail.com>
2023-01-25 15:37:30 +0100

    auto

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct dati_auto
@@ -6,20 +7,9 @@ struct dati_auto
     string categoria, marca, modello, colore;
     char lun, mar, mer, gio, ven, sab, dom;
     };
-
-void leggi_file(dati_auto vet_auto[])
+void carica_vet(dati_auto vet_auto[])
 {
-
-    cout<<"Questa è la lista: "<<endl;
-    for (int i=0; i<7; i++)
-    {
-        cout<<endl<<vet_auto[i].categoria<<" "<<vet_auto[i].marca<<" "<<vet_auto[i].modello<<" "<<vet_auto[i].colore<<" ";
-        cout<<" "<<vet_auto[i].lun<<" "<<vet_auto[i].mar<<" "<<vet_auto[i].mer<<" "<<vet_auto[i].gio<<" "<<vet_auto[i].ven<<" "<<vet_auto[i].sab<<" "<<vet_auto[i].dom;
-    }
-}
-void carica_vet(dati_auto vet_autol[])
-{
-    ifstream fin("auto.txt", ios::in);
+    ifstream fin("auto.csv", ios::in);
     string appoggio;
     while(!fin.eof())
     {
@@ -32,7 +22,35 @@ void carica_vet(dati_auto vet_autol[])
     }
     fin.close();
 }
-
+void leggi_file(dati_auto vet_auto[])
+{
+    cout<<"Questa è la lista: "<<endl;
+    for (int i=0; i<7; i++)
+    {
+        cout<<endl<<vet_auto[i].categoria<<" "<<vet_auto[i].marca<<" "<<vet_auto[i].modello<<" "<<vet_auto[i].colore<<" ";
+        cout<<" "<<vet_auto[i].lun<<" "<<vet_auto[i].mar<<" "<<vet_auto[i].mer<<" "<<vet_auto[i].gio<<" "<<vet_auto[i].ven<<" "<<vet_auto[i].sab<<" "<<vet_auto[i].dom;
+    }
+}
+void scrivi_file(dati_auto vet_auto[])
+{
+    ofstream fout("auto.csv");
+    for (int i=0; i<7; i++)
+    {
+        fout<<vet_auto[i].categoria<<" "<<vet_auto[i].marca<<" "<<<<vet_auto[i].modello<<" "<<vet_auto[i].colore<<" ";
+        fout<<vet_auto[i].lun<<" "<<vet_auto[i].mar<<" "<<vet_auto[i].mer<<" "<<vet_auto[i].gio<<" "<<vet_auto[i].ven<<" "<<vet_auto[i].sab<<" "<<vet_auto[i].dom<<" "
+    }
+}
+void lettura_file_aggiornato(dati_auto vet_auto[])
+{
+    cout<<"\nLA LISTA DELLE AUTO PRESENTI NELL'AUTONOLEGGIO SONO: "<<endl;
+    {
+        for (int i=0; i<7; i++)
+        {
+            cout<<vet_auto[i].categoria<<" "<<vet_auto[i].marca<<" "<<vet_auto[i].modello<<" "<<vet_auto[i].colore<<" ";
+            cout<<" "<<vet_auto[i].lun<<" "<<vet_auto[i].mar<<" "<<vet_auto[i].mer<<" "<<vet_auto[i].gio<<" "<<vet_auto[i].ven<<" "<<vet_auto[i].sab<<" "<<vet_auto[i].dom;
+        }
+    }
+}
 void menu()
 {
     int scelta;
